8407c5dbcdee6ec7e4eead57e61f3d0fea431c5b
paityn <paityn626@hotmail.com>
2019-06-26 14:37:10 -0400

    fixed

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ app.listen(3000, function(err){
         console.log(err)
     console.log('Server is live on port 3000')
 });
+
+app.set("view engine", "ejs"); // setting up templating engine
+
+app.get('/home', function (req, res) {
+    res.render('home');
+  })
+  
