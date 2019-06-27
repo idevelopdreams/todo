1858d0914ffd8355552836c8bd02943c81c9448c
carlitoes <2004carlito@gmail.com>
2019-06-27 14:42:41 -0400

    form is accepting task items with the help of body parser

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({extended:false})
+
+const dummyData = [];
+
+// setting up
 const app = express();
+
+
+
 // setting template emgine
 
 app.set("view engine","ejs");
@@ -7,9 +16,20 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
+
+// ############# Routes ##############
+
+// Get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
     res.render('tasks');
   });
+
+  // Post for tasks: posting a task
+  app.post('/tasks', urlEncoded, function(req, res){
+
+    dummyData.push(req.body.task)
+    console.log(dummyData)
+  })
 
   app.listen(3000, function(err){
     if (err)
