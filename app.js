c9abdcd30078a93f9584163759806520ca678471
keybleach <diego301003@icloud.com>
2019-06-27 13:05:17 -0400

    server handaling for handling static files

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 
 app.set("view engine","ejs");
+// USE MIDDLE WARE TO SERVE STATIC FILES 
+app.use(express.static('./public'));
 
 app.get('/tasks', function (req, res) {
     res.render('tasks');
