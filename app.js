62700c38e5fdd288e5660923836084f9675aa0e4
xLaki <brandonbedard12@gmail.com>
2019-06-27 14:38:36 -0400

    done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
+const urlEncoder = bodyParser.urlencoded({entended: false});
+const dummyData = [];
 
 app.set("view engine","ejs");
 
@@ -8,6 +11,12 @@ app.use(express.static('./public'));
 
 app.get('/tasks', function (req, res) {
     res.render('tasks');
+  });
+
+  // Post for tasks: posting a task
+  app.post('/tasks', urlEncoder, function(req, res){
+    dummyData.push(req.body.task);
+    console.log(dummyData);
   });
 
   app.listen(3000, function(err){
