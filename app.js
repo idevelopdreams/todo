b499173a1441ea50d67c17650236e40764f1d987
Alex <alexwolfenden@outlook.com>
2019-06-27 22:39:46 +0400

    finished for today

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({extended: false})
+
+const dummydata = []; 
+
+//setting up
 const app = express();
 
 //setting template engine
@@ -7,8 +13,19 @@ app.set("view engine","ejs");
 //use middleware to serve static files
 app.use(express.static('./public'));
 
+
+//########### ROUTES ############
+
+//Get for Tasks: returns all tasks
 app.get('/tasks', function (req, res) {
     res.render('tasks');
+  });
+
+  // Post for tasks: posting a task
+  app.post('/tasks', urlEncoded, function(req, res){
+  
+    dummydata.push(req.body.task)
+    console.log(dummydata)
   });
 
   app.listen(3000, function(err){
