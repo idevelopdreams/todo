e17eeadcb564c9b937271dd4ddb81ccaee4597e3
paityn <paityn626@hotmail.com>
2019-06-27 14:38:51 -0400

    fix 3.0

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({entended: false})
+
+let dummyData = [];
+
+// setting up 
 const app = express();
 
 app.set("view engine","ejs");
@@ -6,9 +12,18 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
+// ============= ROUTES ====================
+
+// Get for tasks: return all tasks
 app.get('/tasks', function (req, res) {
     res.render('tasks');
   });
+
+// Post for taks: posting a task
+app.post('/tasks', urlEncoded, function(req, res){
+  dummyData.push(req.body.task);
+  console.log(dummyData);
+});
 
   app.listen(3000, function(err){
     if (err)
