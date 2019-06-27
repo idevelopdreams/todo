3e7bb8999daddec50c974fc40794d6d195e2b447
Isaiah <isaiah.adams7895@gmail.com>
2019-06-27 13:35:20 +0430

    creating

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 app.set("view engine","ejs");
+
+//use middleware to serve static filesl semiddleawre to serve static files use middle ware to server static files use middle ware to serve static files use middle ware to serve static files use middle eware to serve static files use middle ware to serve static filse use middle ware to serve use middleware to serve static files use middleware to serve statle use middle ware to serve static files use middleawre to serve tatic files use middle ware to server static file suse middle ware to serve static files use middle ware to serve static files use middle waer to srve 
+app.use(express.static('./public'));
 
 app.get('/tasks', function (req, res) {
     res.render('tasks');
