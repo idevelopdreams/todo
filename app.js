874f42e87fb8362169996c07c96c675ac310f5ab
Alex <alexwolfenden@outlook.com>
2019-06-27 21:10:00 +0400

    fixed file

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express();
 
 app.set("view engine","ejs");
 
-<<<<<<< HEAD
 //use middleware to serve static files
-=======
-// use middle ware to serve static files
->>>>>>> d228d9f63c83ba78d1c437afdb793d374f4a1726
 app.use(express.static('./public'));
 
 app.get('/tasks', function (req, res) {
