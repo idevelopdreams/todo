4d769b43a058ff3100f1d7702985d36605c673d4
pablo <pabloocampopita@gmail.com>
2019-06-27 13:05:03 -0400

    server handling request for static

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.set("view engine","ejs");
+
+// use middle ware to serve static file
+app.use(express.static('./public'));
 
 app.get('/tasks', function (req, res) {
     res.render('tasks');
