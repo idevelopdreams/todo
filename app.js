9a5a5cdf0df72b12ce460c0c1d887af863398d9c
Bren <captainbmq@gmail.com>
2019-06-27 14:04:34 -0400

    spicy

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const urlEncoded = bodyParser.urlencoded({extended: false});
+
 const app = express();
 
 // setting up temp engine
@@ -7,11 +10,20 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
-app.get('/tasks', function (req, res) {
-    res.render('tasks');
-  });
 
-  app.listen(3000, function(err){
+// ######## ROUTES ##############
+
+// get for tasks 
+app.get('/tasks', function (req, res){
+    res.render('tasks');
+});
+
+// post for tasks
+app.post('/tasks',function(req, res){
+  console.log(req)
+});
+
+app.listen(3000, function(err){
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')
