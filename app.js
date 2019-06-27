c97037ccf34b5371ff0c10a049e75a6fe25ebd6b
keybleach <diego301003@icloud.com>
2019-06-27 14:41:32 -0400

    getting information

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express')
+const  bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({extended:false})
+
+const dummyData = [];
+// starting up 
 const app = express();
 
 app.set("view engine","ejs");
 // USE MIDDLE WARE TO SERVE STATIC FILES 
 app.use(express.static('./public'));
 
-// use middle ware to serve static files
-app.use(express.static('./public'));
+// ###### Routes #########
 
+
+// Get for task ; return all task 
 app.get('/tasks', function (req, res) {
     res.render('tasks');
   });
 
-  app.listen(3000, function(err){
+  //post for task ; posting a task 
+  app.post ('/tasks', urlEncoded ,function(req,res){
+    dummyData.push(req.body.tasks)
+    console.log(dummyData)
+  });
+
+  app.listen(4000, function(err){
     if (err)
         console.log(err)
-    console.log('Server is live on port 3000')
+    console.log('Server is live on port 4000')
 })
