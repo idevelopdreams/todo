9b7db612966739a0e9a4d758eb85104fd6461211
Connor_Murray <connormurray215@gmail.com>
2019-06-27 01:51:50 -0400

    dasd

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
+
+// #################### ROUTES ##################
+
+// get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
     res.render('tasks');
   });
 
-  app.listen(3000, function(err){
+// post for tasks: posting a task
+app.post('/tasks', function(req, res){
+  console.log('YOOOOO Post route got HIT!!!!')
+});
+
+app.listen(3000, function(err){
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')
