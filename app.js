5fc7dc82f85f0b1cc19e5247da9b2c4eef164c3e
keybleach <diego301003@icloud.com>
2019-07-01 10:17:48 -0400

    making the ninja app for the server

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require('express')
-<<<<<<< HEAD
-const  bodyParser = require('body-parser')
-const urlEncoded = bodyParser.urlencoded({extended:false})
-
-const dummyData = [];
-// starting up 
-=======
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
 
 const dummyData = [];
 
 // setting up
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
 const app = express();
 
 // setting template engine
@@ -20,18 +12,11 @@ app.set("view engine","ejs");
 // USE MIDDLE WARE TO SERVE STATIC FILES 
 app.use(express.static('./public'));
 
-<<<<<<< HEAD
-// ###### Routes #########
-
-
-// Get for task ; return all task 
-=======
 
 
 // ############### ROUTES ##############
 
 // Get for tasks: returns all tasks
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
 app.get('/tasks', function (req, res) {
     res.render('tasks');
 });
@@ -44,17 +29,7 @@ app.post('/tasks', urlEncoded, function(req, res){
 
 });
 
-<<<<<<< HEAD
-  //post for task ; posting a task 
-  app.post ('/tasks', urlEncoded ,function(req,res){
-    dummyData.push(req.body.tasks)
-    console.log(dummyData)
-  });
-
-  app.listen(4000, function(err){
-=======
 app.listen(3000, function(err){
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
     if (err)
         console.log(err)
     console.log('Server is live on port 4000')
