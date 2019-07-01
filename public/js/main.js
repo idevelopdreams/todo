4efd3a95627581327d4a7f86c07a01b6f6169b98
Jordan <jtfranklin04203@gmail.com>
2019-07-01 14:16:32 -0400

    getting this settings to work

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-const taskItem = document.getElementsByTagName('li');
-
-
-for (let i = 0; i < taskItem.length; i++) {
-    const element = taskItem[i];
-    element.addEventListener('click', function(){
-        fetch('/tasks/' + taskItem[0].id, {method: "delete"} )
-        .then(function(response){
-            // console.log(res.jsoon());
-            return response.json()
-        })
-            console.log(response);
-        })
-    );
-=======
 const taskItems = document.getElementsByTagName('li');
 
 for (let i = 0; i < taskItems.length; i++) {
@@ -35,4 +19,3 @@ for (let i = 0; i < taskItems.length; i++) {
 
 
 
->>>>>>> dff91661a793fe346dbfa545aad9dbcd9b936b44
