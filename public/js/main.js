b538cdb73ef1f27b1fa005dbde293310c6633616
xLaki <brandonbedard12@gmail.com>
2019-07-01 14:15:14 -0400

    hitting delete route route via ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,17 @@
 const taskItems = document.getElementsByTagName('li')
 
-taskItems[0].addEventListener('click', function(){
-    fetch('/tasks/' + taskItems[0].id, {method: "delete"})
-    .then(function(res){
-        console.log(res);
-        return res.json();
+for (let i=0, i < taskItems.length, i++){
+    taskItems[i].addEventListener('click', function(){
+        debugger
+        fetch('/tasks/' + taskItems[i].id, {method: "delete"})
+        .then(function(res){
+            console.log(res);
+            return res.json();
+        });
+        .then(function(data){
+            location.reload();
+        });
     });
-    .then(function(data){
-        console.log(data);
-    });
-});
+};
+
+
