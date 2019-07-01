595b0fcd2c4e9652e5995d99c3518a7bf8257810
xLaki <brandonbedard12@gmail.com>
2019-07-01 11:23:33 -0400

    thanks

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const express = require('express')
-const app = express();
-const bodyParser = require('body-parser');
-const urlEncoder = bodyParser.urlencoded({entended: false});
-const dummyData = [{taskItem: 'XS'}, {taskItem: 'Y'}, {taskItem: 'XYZ'}];
+const bodyParser = require('body-parser')
+const urlEncoded = bodyParser.urlencoded({extended: false})
 
+const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime" },{taskItem: "Sleep" }];
+
+// setting up
+const app = express();
+
+// setting template engine
 app.set("view engine","ejs");
 
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
+
+
+// ############### ROUTES ##############
+
+// Get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
-    res.render('tasks', {taskToDo: dummyData});
-  });
+   res.render('tasks', {taskToDo: dummyData});
+});
 
-  // Post for tasks: posting a task
-  app.post('/tasks', urlEncoder, function(req, res){
-    dummyData.push(req.body.task);
-    console.log(dummyData);
-  });
+// Post for tasks: posting a task
+app.post('/tasks', urlEncoded, function(req, res){
+   let incomingItem = {}
+   incomingItem.taskItem = req.body.task
+   dummyData.push(incomingItem)
+   console.log(dummyData)
+   res.redirect('/tasks')
 
-  app.listen(3000, function(err){
-    if (err)
-        console.log(err)
-    console.log('Server is live on port 3000')
-})
+});
+
+app.listen(3000, function(err){
+   if (err)
+       console.log(err)
+   console.log('Server is live on port 3000')
+});
