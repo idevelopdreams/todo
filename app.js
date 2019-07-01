0e43ad4ef6f341409d6f8c876b58cf9d9cf3f77e
Jordan <jtfranklin04203@gmail.com>
2019-07-01 09:17:23 -0400

    doing another example

--- app.js
+++ app.js
@@ -8,15 +8,11 @@ const dummyData = [];
 const app = express();
 const urlEncoded = bodyphaser.urlencoded({extended: false})
 
-<<<<<<< HEAD
 // setting up
 const app = express()
 
 
 // setting up template engine 
-=======
-// setting template engine
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
 app.set("view engine","ejs");
 
 
@@ -24,28 +20,10 @@ app.set("view engine","ejs");
 app.use(express.static('./public'));
 
 
-<<<<<<< HEAD
 app.get('/tasks', function (req, res) {
     res.render('tasks')
     console.log(req);
   });
-=======
-
-// ############### ROUTES ##############
-
-// Get for tasks: returns all tasks
-app.get('/tasks', function (req, res) {
-    res.render('tasks');
-});
-
-// Post for tasks: posting a task
-app.post('/tasks', urlEncoded, function(req, res){
-
-  dummyData.push(req.body.task)
-  console.log(dummyData)
-
-});
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
 
 app.listen(3000, function(err){
     if (err)
