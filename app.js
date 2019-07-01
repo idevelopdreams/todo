62f1c7a965f3269168b4caa523e884dcc81a9b46
Bren <captainbmq@gmail.com>
2019-07-01 10:19:48 -0400

    finished front end

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const bodyParser = require('body-parser');
-const urlEncoded = bodyParser.urlencoded({extended: false});
-
-let dummydata = [];
-
-const app = express();
-
-// setting up temp engine
-=======
 const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
@@ -19,28 +8,12 @@ const dummyData = [];
 const app = express();
 
 // setting template engine
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
 app.set("view engine","ejs");
 
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
 
-<<<<<<< HEAD
-// ######## ROUTES ##############
-
-// get for tasks 
-app.get('/tasks', urlEncoded, function (req, res){
-    res.render('tasks');
-});
-
-// post for tasks
-app.post('/tasks', urlEncoded, function(req, res){
-
-  dummydata.push(req.body.task);
-  console.log(dummydata);
-  
-=======
 
 // ############### ROUTES ##############
 
@@ -55,7 +28,6 @@ app.post('/tasks', urlEncoded, function(req, res){
   dummyData.push(req.body.task)
   console.log(dummyData)
 
->>>>>>> a1234abf2f95b710fdabacd56d6fc3d13104b6c0
 });
 
 app.listen(3000, function(err){
