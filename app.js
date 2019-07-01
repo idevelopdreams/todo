e366a317431c02bd832175411e0fef91d9dff1bb
keybleach <diego301003@icloud.com>
2019-07-01 13:14:53 -0400

    created delete route to deectect arrary

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
 
-const dummyData = [];
+const dummyData = [{taskItem: "Work on my portfolio" },{taskItem:"Code and watch anime "},{taskItem:"Sleep"}];
 
 // setting up
 const app = express();
@@ -18,19 +18,24 @@ app.use(express.static('./public'));
 
 // Get for tasks: returns all tasks
 app.get('/tasks', function (req, res) {
-    res.render('tasks');
+    console.log('hitting get route ')
+    res.render('tasks',{taskToDo: dummyData});
 });
 
 // Post for tasks: posting a task
 app.post('/tasks', urlEncoded, function(req, res){
+    console.log('hitting post route ');
+    let incomingItem = {}
+    incomingItem.taskItem = req.body.task 
 
-  dummyData.push(req.body.task)
+  dummyData.push(incomingItem)
   console.log(dummyData)
+  res.redirect('/tasks')
 
 });
-
-app.listen(3000, function(err){
-    if (err)
-        console.log(err)
-    console.log('Server is live on port 4000')
-})
+app.delete("/tasks/:id", function(req,res){
+    // console.log(req.params.id);
+    dummyData.splice(req.params.id,1);
+    // console.log(dummyData);
+    res.redirect('/tasks')
+});
