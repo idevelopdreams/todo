288b0fc11b757cb3d8bf901b01556fa972654b9e
Jordan <jtfranklin04203@gmail.com>
2019-07-02 10:08:29 -0400

    making this code right

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
-<<<<<<< HEAD
 
 
 
@@ -38,28 +37,6 @@ app.get('/tasks', function (req, res) {
 
   app.delete("/tasks/:id", function(req, res){
     // console.log(req.params.id);
-=======
-// ############### ROUTES ##############
-
-// Get for tasks: returns all tasks
-app.get('/', (req, res) => {
-    // rendering tasks view and passing taskToDo data
-    res.render('tasks.ejs', {taskToDo: dummyData});
-});
-
-// Post for tasks: posting a task
-app.post('/tasks', urlEncoded, (req, res) => {
-// formatting for incoming data to add to my data set
-  let incomingItem = {};
-  incomingItem.taskItem = req.body.task;
-  dummyData.push(incomingItem);
-  res.redirect('/')
-});
-
-// Delete for task: deleting specify task
-app.delete("/tasks/:id", (req, res) => {
-    // deleteing item from data set
->>>>>>> 2deeac38529ffdc06bfb703875ba40ce9d80ccd9
     dummyData.splice(req.params.id, 1);
     res.json(dummyData)
 >>>>>>> dff91661a793fe346dbfa545aad9dbcd9b936b44
