dc128265b011aa0474aeb657cc86d99b6698c00b
carlitoes <2004carlito@gmail.com>
2019-07-02 10:03:22 -0400

    idk

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const urlEncoded = bodyParser.urlencoded({extended:false})
-+6
-const urlEncoded = bodyParser.urlencoded({extended: false})
-
 
 const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime"},{taskItem: "Sleep"}];
 
@@ -22,29 +19,29 @@ app.use(express.static('./public'));
 // ############### ROUTES ##############
 
 // Get for tasks: returns all tasks
-app.get('/tasks', function (req, res) {
-    console.log("hitting get route");
+app.get('/', (req, res) => {
+    // rendering tasks view and passing taskToDo data
     res.render('tasks', {taskToDo: dummyData});
 });
 
 // Post for tasks: posting a task
-app.post('/tasks', urlEncoded, function(req, res){
-  console.log("hitting Post route");
-  let incomingItem = {}
-  incomingItem.taskItem = req.body.task
-  dummyData.push(incomingItem)
-  console.log(dummyData)
-  res.redirect('/tasks')
+app.post('/tasks', urlEncoded, (req, res) => {
+//   formatting for incoming data ti addd to my data set
+  let incomingItem = {};
+  incomingItem.taskItem = req.body.task;
+  dummyData.push(incomingItem);
+  res.redirect('/');
 });
+// delete for task: deleting specify task
 
-app.delete("/tasks/:id", function(req, res){
-    // console.log(req.params.id);
+app.delete("/tasks/:id", (req, res) => {
+    // deleting item from data set
     dummyData.splice(req.params.id, 1);
-    // console.log(dummyData);
     res.json(dummyData)
 });
 
-app.listen(3000, function(err){
+
+app.listen(3000, (err) => {
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')
