3bb1e4765c607827f72752835782f8794622f165
Alex <alexwolfenden@outlook.com>
2019-07-02 18:03:26 +0400

    added to file to make sure its required to have a task before being able to post, as well as simplifying code in new syntax in es6

--- app.js
+++ app.js
@@ -13,35 +13,29 @@ app.set("view engine","ejs");
 //use middleware to serve static files
 app.use(express.static('./public'));
 
-
-
 //########### ROUTES ############
 
 //Get for Tasks: returns all tasks
-app.get('/tasks', function (req, res) {
-    console.log("hitting get route");
+app.get('/', (req, res) => {
+    //rendering tasks view and passing taskToDo data
     res.render('tasks', {taskToDo: dummyData});
 });
 
 // Post for tasks: posting a task
 app.post('/tasks', urlEncoded, function(req, res){
-  console.log("hitting Post route");
+  //formatting for incoming data to add to my dataset
   let incomingItem = {}
   incomingItem.taskItem = req.body.task
   dummyData.push(incomingItem)
-  console.log(dummyData)
-  res.redirect('/tasks')
+  res.redirect('/')
 });
 
+//Delete for tasks: deleting specific tasks
 app.delete("/tasks/:id", function(req, res){
-    // console.log(req.params.id);
+    //deleting item from data set
     dummyData.splice(req.params.id, 1);
-    // console.log(dummyData);
     res.json(dummyData)
 });
-
-
-
 
   app.listen(3000, function(err){
     if (err)
