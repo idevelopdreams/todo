1e54cc11cc38791e1a7b4b3a17f4f89b849eb606
pablo <pabloocampopita@gmail.com>
2019-07-02 10:02:18 -0400

    adding refactoring using new es6 syntax

--- app.js
+++ app.js
@@ -14,29 +14,27 @@ app.set("view engine","ejs");
 app.use(express.static('./public'));
 
 
-
 // ############### ROUTES ##############
 
 // Get for tasks: returns all tasks
-app.get('/tasks', function (req, res) {
-    console.log("hitting get route");
-    res.render('tasks', {taskToDo: dummyData});
+app.get('/', (req, res) => {  res.render('tasks.ejs', {taskToDo: dummyData});
 });
+
+//rendering tasks and view 
 
 // Post for tasks: posting a task
-app.post('/tasks', urlEncoded, function(req, res){
-  console.log("hitting Post route");
-  let incomingItem = {}
-  incomingItem.taskItem = req.body.task
-  dummyData.push(incomingItem)
-  console.log(dummyData)
-  res.redirect('/tasks')
+app.post('/tasks', urlEncoded,(req, res) => {
+// formatting for incoming data to add to my data set
+  let incomingItem = {};
+  incomingItem.taskItem = req.body.task;
+  dummyData.push(incomingItem);
+  res.redirect('/')
 });
 
-app.delete("/tasks/:id", function(req, res){
-    // console.log(req.params.id);
+//Delete for tasks: deleting specitfy tasks
+app.delete("/tasks/:id", (req, res) => {
+    // deleting item from data set
     dummyData.splice(req.params.id, 1);
-    // console.log(dummyData);
     res.json(dummyData)
 });
 
