9c4a8b3ac5a8b2bcca3e0732b1817e66b14fe6e8
xLaki <brandonbedard12@gmail.com>
2019-07-09 11:22:08 -0400

    i dont even know anymore this shit is giving me the same damn error and nothing is showing up

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin2',
+  password : 'student',
+  database : 'ninjatasker'
+})
+
+db.connect(function(err){
+  if (err) throw err
+  console.log('database connected')
+});
 
 const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime" },{taskItem: "Sleep" }];
 
@@ -19,17 +32,33 @@ app.use(express.static('./public'));
 
 // Get for tasks: returns all tasks
 app.get('/tasks', (req, res) => {
-  // Rendering task view and passing taskToDo data
-  res.render('tasks', {taskToDo: dummyData});
+  let sql = 'SELECT * FROM task';
+  db.query(sql, function (err, results){
+    if (err) throw err;
+    // Rendering task view and passing taskToDo data
+    res.render('tasks', {taskToDo: results});
+  });
 });
+// STORAGE: 'tasks', {taskToDo: dummyData}
+
 
 // Post for tasks: posting a task
 app.post('/tasks', urlEncoded, function(req, res){
+  let task = {taskItem: req.body.task};
+  console.log(req.body);
+  let sql = 'INSERT INTO task SET ?';
+  db.query(sql, task, function (err, results){
+    if (err) throw err;
+    // Rendering task view and passing taskToDo data
+    console.log('task added')
+    res.render('tasks', {taskToDo: results});
+  });
+
   // formatting for incoming data to add to data set
-  let incomingItem = {};
-  incomingItem.taskItem = req.body.task;
-  dummyData.push(incomingItem);
-  res.redirect('/tasks');
+  // let incomingItem = {};
+  // incomingItem.taskItem = req.body.task;
+  // dummyData.push(incomingItem);
+  // res.redirect('/');
 });
 
 app.delete("/tasks/:id", function(req, res){
@@ -39,7 +68,7 @@ app.delete("/tasks/:id", function(req, res){
 });
 
 app.listen(3000, function(err){
-  if (err)
+  if (err) {}
     console.log(err);
   console.log('Server is live on port 3000');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 const taskItems = document.getElementsByTagName('li')
 
 for (let i=0; i < taskItems.length; i++){
-    taskItems[i].addEventListener('click', function(){
-        debugger
-        fetch('/tasks/' + taskItems[i].id, {method: "delete"})
-        .then((res) => res.json())
-        .then((data) => location.reload());
+    const element = taskItems[i];
+
+    element.addEventListener('click', function(){
+
+        fetch('/tasks/' + element.id, {method: "delete"} )
+        .then( (res) => res.json() )
+        .then( (data) => location.reload() );
     });
 };
 
