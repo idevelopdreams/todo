e9ed68bad72aee20626142f6fde3724baefe1737
Connor_Murray <connormurray215@gmail.com>
2019-07-08 22:40:10 -0400

    NOt working

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const urlEncoded = bodyParser.urlencoded({extended: false})
+const mysql = require('mysql')
+
+// connecting to database
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'admin',
+    password : 'student',
+    database : 'ninjatasker'
+  });
+  
+  db.connect(function(err){
+    if (err) throw err;
+    console.log("DB is connected..")
+  });
 
 const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime"},{taskItem: "Sleep"}];
-
+const api = express();
 // setting up
 const app = express();
 
@@ -17,18 +31,30 @@ app.use(express.static('./public'));
 
 // Get for tasks: returns all tasks
 app.get('/', (req, res) => {
-    // rendering tasks view and passing taskToDo data
-    res.render('tasks.ejs', {taskToDo: dummyData});
+    let sql = 'SELECT * FROM task'
+    db.query(sql, function (err, results) {
+        if (err) throw err;
+         // rendering tasks view and passing taskToDo data
+        res.render('tasks.ejs', {taskToDo: results});
+      });
 });
+
 
 // Post for tasks: posting a task
 app.post('/tasks', urlEncoded, (req, res) => {
+    let task = {taskItem: req.body.task}
+    let sql = 'INSERT INTO SET ?';
+    db.query(sql,task,(err, result) =>{
+        if(err) throw err;
+        console.log(results)
+        res.redirect('/')
+})
 // formatting for incoming data to add to my data set
   let incomingItem = {};
   incomingItem.taskItem = req.body.task;
-  dummyData.push(incomingItem);
-  res.redirect('/')
+  dummyData.push(incomingItem)
 });
+
 
 // Delete for task: deleting specify task
 app.delete("/tasks/:id", (req, res) => {
