20b630c85c99ad9bf587d852390423e9339b3573
Connor_Murray <connormurray215@gmail.com>
2019-07-08 23:22:00 -0400

    Posting to database working

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const urlEncoded = bodyParser.urlencoded({extended: false})
-const mysql = require('mysql')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-// connecting to database
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'admin',
-    password : 'student',
-    database : 'ninjatasker'
-  });
-  
-  db.connect(function(err){
-    if (err) throw err;
-    console.log("DB is connected..")
-  });
+const dataBase = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'student',
+  database : 'ninjatasker'
+});
+
+dataBase.connect((err) => {
+  if (err) throw err;
+  console.log("DB is Connected")
+});
+
+const urlEncoded = bodyParser.urlencoded({extended: false})
 
 const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime"},{taskItem: "Sleep"}];
-const api = express();
+
 // setting up
 const app = express();
 
 // setting template engine
 app.set("view engine","ejs");
+
 
 // use middle ware to serve static files
 app.use(express.static('./public'));
@@ -31,40 +32,36 @@ app.use(express.static('./public'));
 
 // Get for tasks: returns all tasks
 app.get('/', (req, res) => {
-    let sql = 'SELECT * FROM task'
-    db.query(sql, function (err, results) {
-        if (err) throw err;
-         // rendering tasks view and passing taskToDo data
-        res.render('tasks.ejs', {taskToDo: results});
-      });
+  let sql = 'SELECT * FROM task';
+  dataBase.query(sql, function (err, results) {
+      if (err) throw err;
+      // rendering tasks view and passing taskToDo data
+      res.render('tasks.ejs', {taskToDo: results});
+    });
 });
-
 
 // Post for tasks: posting a task
 app.post('/tasks', urlEncoded, (req, res) => {
-    let task = {taskItem: req.body.task}
-    let sql = 'INSERT INTO SET ?';
-    db.query(sql,task,(err, result) =>{
-        if(err) throw err;
-        console.log(results)
-        res.redirect('/')
-})
 // formatting for incoming data to add to my data set
-  let incomingItem = {};
-  incomingItem.taskItem = req.body.task;
-  dummyData.push(incomingItem)
+  let task = req.body
+  let sql = 'INSERT INTO task SET ?'
+  dataBase.query(sql, task, function (err, results){
+      if (err) throw err;
+      res.redirect('/')
+      console.log(results)
+  });
 });
-
 
 // Delete for task: deleting specify task
 app.delete("/tasks/:id", (req, res) => {
-    // deleteing item from data set
-    dummyData.splice(req.params.id, 1);
-    res.json(dummyData)
+  // deleteing item from data set
+  dummyData.splice(req.params.id, 1);
+  res.json(dummyData)
 });
 
+// Listening for request and port 3000
 app.listen(3000, function(err){
-    if (err)
-        console.log(err)
-    console.log('Server is live on port 3000')
+  if (err)
+      console.log(err)
+  console.log('Server is live on port 3000')
 })
