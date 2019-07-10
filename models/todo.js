a5efcd97bebee11469970a88baef2203ddda0d77
paityn <paityn626@hotmail.com>
2019-07-10 10:49:28 -0400

    mcv update for the todo app

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'admin',
+    password : 'student',
+    database : 'ninjatasker'                                                                            
+
+});
+
+db.connect(function(err){
+    if (err) throw err;
+    console.log("DB is connected ...");
+});
+
+module.exports = db;
