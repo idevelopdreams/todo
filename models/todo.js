f534cbfa4d64120602f685325860cf2a4e6c3469
carlitoes <2004carlito@gmail.com>
2019-07-10 10:47:51 -0400

    commiting to repo

---
+++ models/todo.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'admin',
+    password : 'student',
+    database : 'NinjaTasker'
+  });
+
+db.connect(function(err){
+    if(err) throw err;
+    console.log("DB is connected...");
+});
+
+module.exports = db;
