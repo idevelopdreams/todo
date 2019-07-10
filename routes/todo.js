f2e21088b3a1dcb931a82f14c2382af4a909744d
Isaiah <isaiah.adams7895@gmail.com>
2019-07-10 19:20:02 +0430

    fixed stuff

--- routes/todo.js
+++ routes/todo.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const todoController = require('../contollers/todo')
+
 
 //parsing form data
 const bodyParser = require('body-parser');
@@ -7,36 +9,11 @@ const urlEncoded = bodyParser.urlencoded({extended: false})
 // ############### ROUTES ##############
 
 // Get for tasks: returns all tasks
-router.get('/', (req, res) => {
-    let squl = 'SELECT * FROM task';
-    db.query(squl, function (err, results, fields) {
-        if (err) throw err;
- // rendering tasks view and passing taskToDo data
-        res.render('tasks', {taskToDo: results});
-});
-   
-});
- 
+router.get('/', todoController.allTodos);
 // Post for tasks: posting a task
-app.post('/tasks', urlEncoded, (req, res) => {
-    let task = req.body
-    let sql = 'INSERT INTO task SET ?' ;
-    db.query(sql, task, function (err, results) {
-        if (err) throw err;
-        // rendering tasks view and passing taskToDo data
-        console.log(results)
-        res.redirect('/')
-    });
-});
+router.post('/tasks', urlEncoded, todoController.addTask);
 
 // Delete for task: deleting specify task
-app.delete("/tasks/:id", (req, res) => {
-    let sql = 'DELETE  FROM task WHERE ID=' + req.params.id;
-    db.query(sql,(err, result) =>{
-        if(err) throw err;
-        console.log(result);
-        res.json(result)
-    })
-});
+router.delete("/tasks/:id", todoController.removeTask);
 
 module.exports = router;
