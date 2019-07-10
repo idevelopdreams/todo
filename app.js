d14cea908f77734fe5685dc1224d236c9e5afe2c
pablo <pabloocampopita@gmail.com>
2019-07-10 10:48:40 -0400

    making some changes

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql');
+const express = require('express');
+const routes = require('./routes');
+const database = require('./models/todo')
 
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'admin',
-    password : 'pablo',
-    database : 'ninjatasker'
-  });
+// starting are app
+const app = express();
 
-db.connect(function(err){
-    if (err) throw err
-    console.log("DB is connected, you good..")
+//adding context to our request
+app.use( (req, res, next ) => {
+    req.context = { db: database }
+    next();
 });
 
-
-const urlEncoded = bodyParser.urlencoded({extended: false})
-
-const dummyData = [{taskItem: "Work on my portfolio" },{taskItem: "Code and watch anime"},{taskItem: "Sleep"}];
-
-// setting up
-const app = express();
+//routing manager
+app.use(routes);
 
 // setting template engine
 app.set("view engine","ejs");
@@ -28,41 +20,6 @@ app.set("view engine","ejs");
 // use middle ware to serve static files
 app.use(express.static('./public'));
 
-// ############### ROUTES ##############
-
-// Get for tasks: returns all tasks
-app.get('/', (req, res) => {
-    let sql = 'SELECT * FROM task'
-    db.query(sql, function (err, results) {
-        if (err) throw err;
-        // rendering tasks view and passing taskToDo data
-        res.render('tasks', {taskToDo: results});
-    }); 
-});
-
-//rendering tasks and view 
-
-// Post for tasks: posting a task
-app.post('/tasks', urlEncoded, (req, res) => {
-    let task = req.body
-    let sql = 'INSERT INTO task SET ?';
-    db.query(sql, task, function (err, results) {
-         if (err) throw err;
-         // rendering tasks view and passing taskToDo data
-         console.log(results)
-         res.redirect('/')
-        });
-});
-
-// Delete for task: deleting specify task
-app.delete("/tasks/:id", (req, res) => {
-    let sql = 'DELETE  FROM task WHERE ID=' + req.params.id;
-    db.query(sql,(err, result) =>{
-        if(err) throw err;
-        console.log(result);
-        res.json(result)
-       })
-    });
 
 app.listen(3000, function(err){
     if (err)
