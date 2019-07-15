0a56998ab7dd8bfde3649572d3b13c917eb6e350
Tony <tony.n.flpa@gmail.com>
2019-07-15 10:14:01 -0400

    implementing encryption of new user password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
-// creating our user model
-// export the model so our index
+const bcrypt = require('bcryptjs')
+// creating our user model//
+// export the model so our index//
 module.exports = function(sequelize, DataTypes){
     var User = sequelize.define("User", {
        email: {
@@ -15,5 +16,19 @@ module.exports = function(sequelize, DataTypes){
            allowNull: false
        }
     });
+
+// creating custom model for user//
+// this will handle if an unhashed password the user is providing is equal to the hash password in our database/
+User.prototype.validate = function(password){
+    return bcrypt.compareSync(password, this.password)
+}
+
+// hooks are various methods that run during our model lifecycle//
+// in this case, before a user is created, we will automatically encrypt password in coming//
+User.addHook("beforeCreate", function(user){
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+})
+
     return User
 }
+
