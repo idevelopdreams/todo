33a08f8ea6677dc9012fa8ab00c37fa256e1a55a
Roger <idevelopedreams@gmail.com>
2019-07-15 10:12:55 -0400

    implementing encryption of new user passwords and a method to compare passwords that are encrypted

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes  = require('./routes');
-const database = require("./models");
+const database = require('./models');
 require('dotenv').config()
 
 
