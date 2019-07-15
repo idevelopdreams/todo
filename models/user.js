18a9f2eeca467bb1427d8588cd651a7ee32f3336
carlitoes <2004carlito@gmail.com>
2019-07-15 10:13:16 -0400

    implementing encryption of new user passwords and a method to compare passwords that are encrypted

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 // creating our user model
 // export the model so our index can use it
 module.exports = function(sequelize, DataTypes){
@@ -16,5 +18,19 @@ module.exports = function(sequelize, DataTypes){
         }
     });
 
-    return User
+// create custom method for our user model.
+// this will handle if an unhashed password the user is providing
+// is equal to the hash password in out database
+User.prototype.validaPassword = function(password){
+    return bcrypt.compareSync(password, this.password)
+}
+
+// Hooks are various methods that run during our model lifecycle
+// in this case, before a user is created, we will automatically
+// encrypt password coming in.
+User.addHook('beforeCreate', function(user){
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+});
+
+    return User;
 }
