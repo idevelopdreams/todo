7105ae41377a0aa63ca2ef852117f60cbc00f714
Connor_Murray <connormurray215@gmail.com>
2019-07-14 23:15:07 -0400

    Went to lunch

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const routes  = require('./routes');
 const database = require("./models");
-require('dotenv').config()
+const session = require('express-session')
+const passport = require('./config/passport');
 
 
 const port = process.env.PORT || 3000 ;
@@ -22,8 +24,11 @@ app.use(routes);
 app.set("view engine","ejs");
 
 
-// use middle ware to serve static files
+// setting up middleware
 app.use(express.static('./public'));
+app.use(session( {secret: 'I love veros cohort 2', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 database.sequelize.sync().then(function(){
---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const db = require('../models')
+// telling passport we want to use local strategy, in other words we want to use
+// email and passwords
+passport.use(new LocalStrategy(
+    function(email, password, done) {
+      db.User.findOne( {where: { email: email } } ).then( function(dbuser){
+        //   if there is no user with the provided email
+        if (!dbuser) {
+            return done(null, false, { message: 'Incorrect Email.' } );
+        }
+        // if there is a suer with that email but the password is wrong
+        else if (!dbuser.validPassword(password)) {
+            return done(null, false, { message: 'Incorrect password.' } );
+        }
+
+        // if none of the above happens, return user
+        return done(null, dbuser);
+      });
+    }
+  ));
